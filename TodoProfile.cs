8a0e266bc9a7f4788d73313e6c274f14e1e792d3
majd0 <majd.ashour@takamul.net.sa>
2021-07-06 16:54:14 +0300

    first commit

---
+++ TodoProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class TodoProfile : Profile
+    {
+        public TodoProfile()
+        {
+            CreateMap<TodoItem, TodoItemDTO>();
+            CreateMap<TodoItemDTO , TodoItem >();
+        }
+    }
+}
